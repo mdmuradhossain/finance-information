cb906ff373c40823f7377a532c55592aa34fa63f
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-01-17 22:39:19 +0600

    project created

---
+++ src/test/java/io/murad/finance/information/FinanceInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.murad.finance.information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
