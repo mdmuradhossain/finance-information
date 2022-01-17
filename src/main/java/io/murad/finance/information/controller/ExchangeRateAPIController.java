a957ded4c0fcc4ef1909e022ce97c99ee456e70e
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-01-17 22:47:08 +0600

    ExchangeRateAPIController added

---
+++ src/main/java/io/murad/finance/information/controller/ExchangeRateAPIController.java
@@ -0,0 +1,9 @@
+package io.murad.finance.information.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController("/api/exchange-rate")
+public class ExchangeRateAPIController {
+}
