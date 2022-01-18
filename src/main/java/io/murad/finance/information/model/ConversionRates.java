9b2335b3c99a412144e2e3fa36eccf442b28f2ae
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-01-18 13:35:41 +0600

    WebMVCConfig added with Cors Origin config

---
+++ src/main/java/io/murad/finance/information/model/ConversionRates.java
@@ -0,0 +1,13 @@
+package io.murad.finance.information.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ConversionRates {
+    private String USD;
+    private String AED;
+}
--- src/main/java/io/murad/finance/information/model/ExchangeRateUSD.java
+++ src/main/java/io/murad/finance/information/model/ExchangeRateUSD.java
@@ -1,19 +1,30 @@
 package io.murad.finance.information.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
+
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
-public class ExchangeRateUSD {
+@JsonIgnoreProperties
+public class ExchangeRateUSD implements Serializable {
+
     private String result; //"success"
     private String documentation;// "https://www.exchangerate-api.com/docs",
-    private String termsOfUse; //"https://www.exchangerate-api.com/terms",
-    private String timeLastUpdateUnix; //1642377601,
-    private String timeLastUpdateUtc; //"Mon, 17 Jan 2022 00:00:01 +0000",
-    private String timeNextUpdateUnix; //1642464001,
-    private String timeNextUpdateUtc; //"Tue, 18 Jan 2022 00:00:01 +0000",
-    private String baseCode; //"USD",
+    private String terms_of_use; //"https://www.exchangerate-api.com/terms",
+    private String time_last_update_unix; //1642377601,
+    private String time_last_update_utc; //"Mon, 17 Jan 2022 00:00:01 +0000",
+    private String time_next_update_unix; //1642464001,
+    private String time_next_update_utc; //"Tue, 18 Jan 2022 00:00:01 +0000",
+    private String base_code; //"USD",
+
+    private ConversionRates conversion_rates;
+
 }
